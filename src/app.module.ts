79ee38fe597bd6eaf2ed1bd480fced0f00c6d984
liamosycka <osyckaliam@gmail.com>
2024-11-30 17:38:41 -0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import { RecipesModule } from './recipes/recipes.module';
+
+@Module({
+  imports: [
+    RecipesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
+})
+export class AppModule {}
